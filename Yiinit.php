ff77b8c93b46e1256173eeb640fbe5a102297684
Ramazan Kamilov <ramazan@kamilov.ru>
2014-04-21 15:01:56 +0400

    FIX. spl_autoload case sensitive

--- Yiinit.php
+++ Yiinit.php
@@ -7,6 +7,10 @@
  *
  */
 set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__DIR__));
-spl_autoload_register();
+spl_autoload_register(function($className) {
+    if(strpos($className, 'yiinit') === 0) {
+        require __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, substr($className, 7)) . '.php';
+    }
+});
 
 class Yiinit extends yiinit\Base {};
